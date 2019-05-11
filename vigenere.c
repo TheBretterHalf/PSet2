dddb375870e62602b918241573428169fa35acb0
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-11 16:37:15 +0000

    vig

--- vigenere.c
+++ vigenere.c
@@ -5,15 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int shift (char c);
+//|| isalpha(argv[1][0])
+
+char ctext[]="";
 
 int main(int argc, string argv[])
 {
-    //printf("%i\n", argc);
-    if (argv[1] == NULL || argc>=3)
+    //printf("%s\n", argv[1]);
+    //argv[1] == NULL || argc>2
+    if (argv[1] == NULL || !isalpha(argv[1][0]) || argc>2)
     {
         //printf("%i\n", argc);
         //printf("%s\n", argv[1]);
+        //printf("%c\n", argv[1][0]);
+        //printf("%i\n", isalpha());
         return 1;
     }
     else
@@ -22,13 +27,34 @@ int main(int argc, string argv[])
         //printf("Success\n");
         for (int i = 0; i < strlen(s); i++)
         {
-            //printf("%c\n", argv[1][i]);
-            for(int j=0; j<strlen(argv[j]); j++)
+            int kcounter = i % strlen(argv[1]);
+            //printf("%i\n", kcounter);
+            if (isupper(s[i]))
             {
-                int c = (int) argv[1][j]+s[j];
-                printf("%i\n", c);
+                //s[i] =(int) (((( s[i]) - 'A') % 26) + 'A') + ((((argv[1][kcounter]) - 'A') % 26) + 'A');
+                s[i] = (((s[i]-'A') + (argv[1][kcounter]-'A'))%26)+'A';
+                //printf("%c\n", s[i]);
             }
+            else if (islower(s[i]))
+            {
+                //printf("%c\t", s[i]);
+                //printf("%i\t", (int) s[i]);
+                //s[i] =(int) (((( s[i]) - 'a') % 26) + 'a') + ((((argv[1][kcounter]) - 'a') % 26) + 'a');
+                s[i] = (((s[i]-'a') + (argv[1][kcounter]-'a'))%26)+'a';
+                //printf("%c\t\n", s[i]);
+            }
+            //printf("%i\n", b);
+            //printf ("%i\n", s[i]);
+            //s[i] = (int) s[i]+cypher;
+            //printf("ciphertext: %s\n", s);
+            else
+            {
+
+            }
+            //printf("%s\n", s);
+            strcpy(ctext,s);
         }
+        printf("ciphertext: %s\n", ctext);
         return 0;
     }
 }
@@ -39,3 +65,5 @@ int main(int argc, string argv[])
 // }
 
 //isdigit(argv[1]) || argc<3
+
+//s[i] =(int) (((( s[i]) - 'A') % 26) + 'A') + ((((argv[1][kcounter]) - 'A') % 26) + 'A');
