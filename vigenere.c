fd109ede1a062194ea5fd0b04470adad6f2c2bcc
Brett Stainken <brettstainken.student@careerdevs.com>
2019-05-11 03:25:00 +0000

    vig

--- vigenere.c
+++ vigenere.c
@@ -2,16 +2,40 @@
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+int shift (char c);
 
 int main(int argc, string argv[])
 {
-    if (argv[1] == NULL ||)
+    //printf("%i\n", argc);
+    if (argv[1] == NULL || argc>=3)
     {
+        //printf("%i\n", argc);
+        //printf("%s\n", argv[1]);
         return 1;
     }
     else
     {
-
+        string s = get_string("plaintext: ");
+        //printf("Success\n");
+        for (int i = 0; i < strlen(s); i++)
+        {
+            //printf("%c\n", argv[1][i]);
+            for(int j=0; j<strlen(argv[j]); j++)
+            {
+                int c = (int) argv[1][j]+s[j];
+                printf("%i\n", c);
+            }
+        }
+        return 0;
     }
+}
+
+// int shift(char c)
+// {
+
+// }
+
+//isdigit(argv[1]) || argc<3
